5584c9b55f539b1de3f886ae51b5cd8b1afb1da8
Aleksey Nikolaev <nikolaev@iprojects.ru>
2021-06-18 10:13:27 +0300

    min

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
+const { terser } = require('rollup-plugin-terser');
 
 module.exports = {
   input: "./src/index.js",
-  output: {
-    file: "./dist/bundle.js",
-    format: "iife"
-  },
+  output: [
+    {
+      file: "./dist/bundle.js",
+      sourcemap: true,
+      format: "iife"
+    },
+    {
+      file: "./dist/bundle.min.js",
+      plugins: [terser()],
+      format: "iife"
+    }
+  ],
   plugins: [nodeResolve()]
 }
